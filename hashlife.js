871e2497caf63a0bf58be7551cff38824c103685
Jeff Fowler <jeffowler@gmail.com>
2019-05-08 13:05:54 -0400

    recursive test case quadtree

--- hashlife.js
+++ hashlife.js
@@ -7,4 +7,54 @@ class Quadtree {
   }
 }
 
-console.log(new Quadtree({nw: 1, ne: 0, se: 1, sw: 0}));
+let test = `
+* . . . . . . . . . . . . . . *
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . .
+* . . . . . . . . . . . . . . *
+`;
+
+const array2Quadtree = function (arr) {
+  if (arr.length === 1) {
+    return arr[0][0]
+  }
+
+  const nw = arr.slice(0, arr.length / 2)
+    .map(subarr => subarr.slice(0, subarr.length / 2))
+
+  const ne = arr.slice(0, arr.length / 2)
+    .map(subarr => subarr.slice(subarr.length / 2, subarr.length))
+
+  const se = arr.slice(arr.length / 2, arr.length)
+    .map(subarr => subarr.slice(subarr.length / 2, subarr.length))
+
+  const sw = arr.slice(arr.length / 2, arr.length)
+    .map(subarr => subarr.slice(0, subarr.length / 2))
+
+  return new Quadtree({
+    nw: array2Quadtree(nw),
+    ne: array2Quadtree(ne),
+    se: array2Quadtree(se),
+    sw: array2Quadtree(sw)
+  });
+};
+
+test = test.split('\n')
+  .slice(1, 17)
+  .map(line => line.split(' ').map(el => (el === '.' ? false : true))),
+
+console.log(
+  array2Quadtree(test).nw.nw.nw
+);
