d22e7945fd6344b2b1f3992e3e12c06d51dad03c
Jeff Fowler <jeffowler@gmail.com>
2019-05-08 16:00:37 -0400

    equality

--- hashlife.js
+++ hashlife.js
@@ -1,9 +1,18 @@
 class Quadtree {
-  constructor({nw, ne, se, sw}) {
+  constructor({nw, ne, se, sw} = {}) {
     this.nw = nw;
     this.ne = ne;
     this.se = se;
     this.sw = sw;
+  }
+
+  equals(quadtree) {
+    const check = direction =>
+      this[direction].constructor.name === 'Quadtree'
+        ? this[direction].equals(quadtree[direction])
+        : this[direction] === quadtree[direction];
+
+    ['nw', 'ne', 'se', 'sw'].map(check).every(x => x === true);
   }
 }
 
@@ -26,35 +35,40 @@ let test = `
 * . . . . . . . . . . . . . . *
 `;
 
-const array2Quadtree = function (arr) {
+const array2Quadtree = function(arr) {
   if (arr.length === 1) {
-    return arr[0][0]
+    return arr[0][0];
   }
 
-  const nw = arr.slice(0, arr.length / 2)
-    .map(subarr => subarr.slice(0, subarr.length / 2))
+  const nw = arr
+    .slice(0, arr.length / 2)
+    .map(subarr => subarr.slice(0, subarr.length / 2));
 
-  const ne = arr.slice(0, arr.length / 2)
-    .map(subarr => subarr.slice(subarr.length / 2, subarr.length))
+  const ne = arr
+    .slice(0, arr.length / 2)
+    .map(subarr => subarr.slice(subarr.length / 2, subarr.length));
 
-  const se = arr.slice(arr.length / 2, arr.length)
-    .map(subarr => subarr.slice(subarr.length / 2, subarr.length))
+  const se = arr
+    .slice(arr.length / 2, arr.length)
+    .map(subarr => subarr.slice(subarr.length / 2, subarr.length));
 
-  const sw = arr.slice(arr.length / 2, arr.length)
-    .map(subarr => subarr.slice(0, subarr.length / 2))
+  const sw = arr
+    .slice(arr.length / 2, arr.length)
+    .map(subarr => subarr.slice(0, subarr.length / 2));
 
   return new Quadtree({
     nw: array2Quadtree(nw),
     ne: array2Quadtree(ne),
     se: array2Quadtree(se),
-    sw: array2Quadtree(sw)
+    sw: array2Quadtree(sw),
   });
 };
 
-test = test.split('\n')
+test = test
+  .split('\n')
   .slice(1, 17)
-  .map(line => line.split(' ').map(el => (el === '.' ? false : true))),
+  .map(line => line.split(' ').map(el => (el === '.' ? false : true)));
 
-console.log(
-  array2Quadtree(test).nw.nw.nw
-);
+const x = array2Quadtree(test);
+const y = array2Quadtree(test);
+console.log(x.equals(y));
